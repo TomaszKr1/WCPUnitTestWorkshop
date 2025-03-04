1e0bf182f8d6f617945cdfffcb59b9db63ceec06
Tomasz Krzywonos <TKrzywonos@idexcorp.com>
2025-03-04 22:23:48 +0100

    Init solution

---
+++ UnitTestWorkshop/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTestWorkshop;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    
+    public int Max(int a, int b) => (a > b) ? a : b;
+    
+    public int Subtract(int a, int b) => a - b;
+    
+    public int Multiply(int a, int b) => a * b;
+    
+    public int Divide(int a, int b) => a / b;
+}
