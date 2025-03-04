ce83ca7621a0ebc5bc49986669bbb6b6567cbd5a
Tomasz Krzywonos <TKrzywonos@idexcorp.com>
2025-03-05 00:38:17 +0100

    Theory parameters

--- UnitTestWorkshop.UnitTests/CalculatorTests.cs
+++ UnitTestWorkshop.UnitTests/CalculatorTests.cs
@@ -1,17 +1,45 @@
+
 namespace UnitTestWorkshop.UnitTests;
 
 public class CalculatorTests
 {
-    [Fact]
-    public void Add_ShouldReturnSumOfTwoNumbers()
+    [Theory]
+    [InlineData(3, 5, 8)]
+    [InlineData(3, 0, 3)]
+    [InlineData(0, 5, 5)]
+    public void Add_WhenCalled_ReturnsSumOfArguments(int a, int b, int expected)
     {
         // Arrange
         var calculator = new Calculator();
-        
+
         // Act
-        var result = calculator.Add(1, 2);
-        
+        int result = calculator.Add(a, b);
+
         // Assert
-        Assert.Equal(3, result);
+        Assert.Equal(expected, result);
+    }
+        
+    [Theory]
+    //[MemberData(nameof(Max_SecondArgumentIsGreater_Data))]
+    //[ClassData(typeof(MaxSecondArgumentIsGreaterDataGenerator))]
+    [ClassData(typeof(MaxSecondArgumentIsGreaterDataGeneratorObject))]
+    public void Max_SecondArgumentIsGreater_ReturnsSecondArgument(int firstNumber, int secondNumber, int expected)
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        int result = calculator.Max(firstNumber, secondNumber);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+
+    public static IEnumerable<TheoryDataRow<int, int, int>> Max_SecondArgumentIsGreater_Data()
+    {
+        yield return new TheoryDataRow<int, int, int>(3, 5, 5);
+        yield return new TheoryDataRow<int, int, int>(3, 0, 3);
+        yield return new TheoryDataRow<int, int, int>(0, 5, 5);
     }
 }
---
+++ UnitTestWorkshop.UnitTests/Max_SecondArgumentIsGreater_DataGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace UnitTestWorkshop.UnitTests;
+
+public class MaxSecondArgumentIsGreaterDataGenerator : TheoryData<int, int, int>
+{
+    public MaxSecondArgumentIsGreaterDataGenerator()
+    {
+        Add(3, 5, 5);
+        Add(3, 0, 3);
+        Add(0, 5, 5);
+        Add(5, 3, 5);
+        Add(0, 3, 3);
+        Add(5, 0, 5);
+        Add(3, 3, 3);
+        Add(0, 0, 0);
+        
+        // AddRange(new TheoryData<int, int, int>
+        // {
+        //     {3, 5, 5},
+        //     {3, 0, 3},
+        //     {0, 5, 5},
+        //     {5, 3, 5},
+        //     {0, 3, 3},
+        //     {5, 0, 5},
+        //     {3, 3, 3},
+        //     {0, 0, 0}
+        // });
+    }
+}
+
+
+public class MaxSecondArgumentIsGreaterDataGeneratorObject : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return ["dupa", 5, 5];
+        yield return [3, 0, false];
+        yield return [0, 5, 5];
+        yield return [5, 3d, 5];
+        yield return [0f, 3, 3];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
